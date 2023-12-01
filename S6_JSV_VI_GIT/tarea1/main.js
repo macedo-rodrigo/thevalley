519d950a000fda32d569d7cf801cc4b536ed82b3
Rodri <rodrigoalvesdemacedo@live.com>
2023-12-01 16:41:54 +0100

    Mejoras tarea 1

--- S6_JSV_VI_GIT/tarea1/main.js
+++ S6_JSV_VI_GIT/tarea1/main.js
@@ -1,20 +1,20 @@
 
 function takeName(){
     // obtener el valor del input
-    console.log('Funcion TakeName llamada');
+    console.log('a ver si sale');
     let input = document.getElementById('input').value
 
     fetch(`https://api.agify.io/?name=${input}`)
         .then(resultado => resultado.json()) // Como el metodo es get, necesito pasar el output a json
         .then(resultado => {
-            let resultPrint = document.getElementById('output'); // Ubico donde quiero pinta el resultado en pantalla
-            resultPrint.textContent = `${resultado.age}`; // Para remplazar el "?" por el resultado
+            let resultPrint = document.getElementById('output'); // Ubico donde quiero pintar el resultado en pantalla
+            resultPrint.textContent = `${resultado.age}`; // Para remplazar el "?" por el resultado. La parte del $ la googleé.
             console.log(resultado.age);
         })
         .catch(error => console.log(error))
 }
 
-document.getElementById('predictButton').addEventListener('click', takeName); // Quise usar el loaded, pero al final me lié y desistí, igual a funcionado.
+document.getElementById('predictButton').addEventListener('click', takeName); // Quise usar el loaded, pero al final me lié y desistí, igual ha funcionado.
 
 
-// Comentario general: sin googlear los errores y buscar soluciones en internet, no hubiese podido hacerlo!!!!
+// Comentario general: sin googlear los errores y buscar soluciones en internet, no hubiese podido hacerlo!
